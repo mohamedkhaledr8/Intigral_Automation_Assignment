fa9aaac2543ec968328acd836f08eda471ef3000
Mohamed Khaled <54446566+mohamedkhaledr8@users.noreply.github.com>
2023-11-02 16:03:34 +0300

    Update SubscribeTest.java

--- stc-tv/src/test/java/Tests/SubscribeTest.java
+++ stc-tv/src/test/java/Tests/SubscribeTest.java
@@ -14,6 +14,7 @@ public class SubscribeTest {
 	 @Test
 	    public void Validate_Packages_Type_And_Price_And_currecny_For_KSA() {
 		 sub.selectCountry("ksa");
+		Assert.assertEquals( sub.getPackageType("lite"),"LITE");
 		Assert.assertEquals( sub.getPackagePrice("lite"),"15");
 		Assert.assertEquals( sub.getPackageCurrency("lite"),"SAR/month");
 		Assert.assertEquals( sub.getPackageType("classic"),"CLASSIC");
@@ -28,6 +29,7 @@ public class SubscribeTest {
 	     @Test
 	    public void Validate_Packages_Type_And_Price_And_currecny_For_Kuwait() {
 			 sub.selectCountry("kw");
+                Assert.assertEquals( sub.getPackageType("lite"),"LITE");
 		Assert.assertEquals( sub.getPackagePrice("lite"),"1.2");
 		Assert.assertEquals( sub.getPackageCurrency("lite"),"KWD/month");
 		Assert.assertEquals( sub.getPackageType("classic"),"CLASSIC");
@@ -43,6 +45,7 @@ public class SubscribeTest {
 	     @Test
 	    public void Validate_Packages_Type_And_Price_And_currecny_For_Bahrain() {
 			 sub.selectCountry("bh");
+		    
 		Assert.assertEquals( sub.getPackageType("lite"),"LITE");
 		Assert.assertEquals( sub.getPackagePrice("lite"),"2");
 		Assert.assertEquals( sub.getPackageCurrency("lite"),"BHD/month");
